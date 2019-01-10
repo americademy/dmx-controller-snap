ac644fc617cf9d850a7e3b17bafe3efe47ae8719
Craig Ulliott <craigulliott@gmail.com>
2019-01-10 15:32:07 -0600

    snap configuration

--- dmx-client/client.c
+++ dmx-client/client.c
@@ -1,5 +1,4 @@
 // use ./client 2 250
-
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,40 +8,42 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NAME "/tmp/dmx.sock"
+char SOCKET_FILE[256];
 
 int main(int argc, char *argv[], char * envp[]) {
+  const char* snap_data_path = getenv("SNAP_DATA");
+  snprintf(SOCKET_FILE, sizeof SOCKET_FILE, "%s/dmx-server.sock", snap_data_path);
 
-    int channel = strtol(argv[1], NULL, 10);
-    int value = strtol(argv[2], NULL, 10);
+  int channel = strtol(argv[1], NULL, 10);
+  int value = strtol(argv[2], NULL, 10);
 
-    printf("Setting channel: %d to %d", channel, value);
+  printf("Setting channel: %d to %d", channel, value);
 
-    int sock;
-    struct sockaddr_un server;
-    sock = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  int sock;
+  struct sockaddr_un server;
+  sock = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
-    if (sock < 0) {
-        perror("opening stream socket");
-        exit(1);
+  if (sock < 0) {
+    perror("opening stream socket");
+    exit(1);
+  }
+
+  server.sun_family = AF_UNIX;
+  strcpy(server.sun_path, SOCKET_FILE);
+
+  if (connect(sock, (struct sockaddr *) &server, sizeof(struct sockaddr_un)) < 0) {
+    close(sock);
+    perror("connecting stream socket");
+    exit(1);
+
+  } else {
+
+    char data[80];
+    sprintf(data, "%d:%d", channel, value);
+
+    if (write(sock, data, sizeof(data)) < 0) {
+      perror("writing on stream socket");
     }
-
-    server.sun_family = AF_UNIX;
-    strcpy(server.sun_path, NAME);
-
-    if (connect(sock, (struct sockaddr *) &server, sizeof(struct sockaddr_un)) < 0) {
-        close(sock);
-        perror("connecting stream socket");
-        exit(1);
-
-    } else {
-
-        char data[80];
-        sprintf(data, "%d:%d", channel, value);
-
-        if (write(sock, data, sizeof(data)) < 0) {
-            perror("writing on stream socket");
-        }
-        close(sock);
-    }
+    close(sock);
+  }
 }
--- dmx-server/server.c
+++ dmx-server/server.c
@@ -8,7 +8,127 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <errno.h>
-#include <pigpio.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#define GPSET0 7
+#define GPCLR0 10
+
+unsigned piModel;
+unsigned piRev;
+
+static volatile uint32_t  *gpioReg = MAP_FAILED;
+
+#define PI_BANK (gpio>>5)
+#define PI_BIT  (1<<(gpio&0x1F))
+
+/* gpio modes. */
+
+#define PI_OUTPUT 1
+
+void gpioSetMode(unsigned gpio, unsigned mode)
+{
+   int reg, shift;
+
+   reg   =  gpio/10;
+   shift = (gpio%10) * 3;
+
+   gpioReg[reg] = (gpioReg[reg] & ~(7<<shift)) | (mode<<shift);
+}
+
+void gpioWrite(unsigned gpio, unsigned level)
+{
+   if (level == 0) *(gpioReg + GPCLR0 + PI_BANK) = PI_BIT;
+   else            *(gpioReg + GPSET0 + PI_BANK) = PI_BIT;
+}
+
+unsigned gpioHardwareRevision(void)
+{
+   static unsigned rev = 0;
+
+   FILE * filp;
+   char buf[512];
+   char term;
+   int chars=4; /* number of chars in revision string */
+
+   if (rev) return rev;
+
+   piModel = 0;
+
+   filp = fopen ("/proc/cpuinfo", "r");
+
+   if (filp != NULL)
+   {
+      while (fgets(buf, sizeof(buf), filp) != NULL)
+      {
+         if (piModel == 0)
+         {
+            if (!strncasecmp("model name", buf, 10))
+            {
+               if (strstr (buf, "ARMv6") != NULL)
+               {
+                  piModel = 1;
+                  chars = 4;
+               }
+               else if (strstr (buf, "ARMv7") != NULL)
+               {
+                  piModel = 2;
+                  chars = 6;
+               }
+               else if (strstr (buf, "ARMv8") != NULL)
+               {
+                  piModel = 2;
+                  chars = 6;
+               }
+            }
+         }
+
+         if (!strncasecmp("revision", buf, 8))
+         {
+            if (sscanf(buf+strlen(buf)-(chars+1),
+               "%x%c", &rev, &term) == 2)
+            {
+               if (term != '\n') rev = 0;
+            }
+         }
+      }
+
+      fclose(filp);
+   }
+   return rev;
+}
+
+int gpioInitialise(void)
+{
+   int fd;
+
+   piRev = gpioHardwareRevision(); /* sets piModel and piRev */
+
+   fd = open("/dev/gpiomem", O_RDWR | O_SYNC) ;
+
+   if (fd < 0)
+   {
+      fprintf(stderr, "failed to open /dev/gpiomem\n");
+      return -1;
+   }
+
+   gpioReg = (uint32_t *)mmap(NULL, 0xB4, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+   close(fd);
+
+   if (gpioReg == MAP_FAILED)
+   {
+      fprintf(stderr, "Bad, mmap failed\n");
+      return -1;
+   }
+   return 0;
+}
+
+
+
 
 // 1 second = 1000 millisecond
 // 1 millisecond = 1000 microsecond
@@ -81,13 +201,11 @@ void sigintHandler(int sig_num) {
   fprintf(f, "Goodbye\n");
   fflush(f);
   // exit cleanly
-  gpioTerminate();
   exit(sig_num);
 }
 
 void gpioSetup() {
   gpioInitialise();
-
   gpioSetMode(OUTPUT_PIN, PI_OUTPUT);
 }
 
@@ -248,7 +366,7 @@ int main() {
   gpioSetup();
 
   // Set the SIGINT (Ctrl-C) signal handler to sigintHandler
-  gpioSetSignalFunc(SIGINT, sigintHandler);
+  signal(SIGINT, sigintHandler);
 
   prepareDmxValues();
 
@@ -261,6 +379,8 @@ int main() {
     transmits++;
 
     if (skipped_bits) {
+      // on failure, push the logic level down in an attempt to skip the last byte (as the stop bits wont be in the correct place)
+      gpioWrite(OUTPUT_PIN, false);
       fails++;
       fprintf(f, "skipped %d bits for transmission %d\n", skipped_bits, transmits);
       fflush(f);
@@ -273,6 +393,11 @@ int main() {
 
     if (fails > 10000) {
       exit(EXIT_FAILURE);
+    }
+
+    // if we're getting a lot of interrupts in a row, then sleep for .1 second every 10th time
+    if (fails >= 10 && fails % 10 == 0) {
+      usleep(100000);
     }
 
     connection = accept(sock, 0, 0);
